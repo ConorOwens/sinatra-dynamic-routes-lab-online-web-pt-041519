5e90998aa61cff9f5b659862bde2cf4f812a5ce1
Conor Owens <conorj.owens@gmail.com>
2019-08-01 05:41:07 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -29,8 +29,8 @@ class App < Sinatra::Base
   
   get '/:operation/:number1/:number2' do
     @op = params[:operation]
-    @n1 = params[:number1]
-    @n2 = params[:number2]
+    @n1 = params[:number1].to_i 
+    @n2 = params[:number2].to_i 
     
     if @op == 'add'
       @n1+@n2
