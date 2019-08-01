90aeb6571f5339971bf084d8321f7ceaf77e35b3
Conor Owens <conorj.owens@gmail.com>
2019-08-01 04:34:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,7 +4,16 @@ class App < Sinatra::Base
   # Write your code here!
   
   get '/reversename/:name'
-    @name = :name 
-    "#{@name."
+    @name = params[:name] 
+    "#{@name.reverse}"
+  end
+  
+  get '/square/:number'
+    @square = params[:number]*params[:number]
+    "#{@square}"
+  end
+  
+  get '/say/:number/:phrase'
+    
 
 end
